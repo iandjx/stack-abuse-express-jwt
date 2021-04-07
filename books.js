dc648aa32b89f02dd4fbae96e510382a4cabda57
Ian de Jesus <ian@eskwelabs.com>
2021-04-07 21:15:06 +0900

    feat: accept github data

--- books.js
+++ books.js
@@ -1,78 +1,77 @@
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const app = express();
 
-const accessTokenSecret = 'somerandomaccesstoken';
+const accessTokenSecret = "somerandomaccesstoken";
 
 app.use(bodyParser.json());
 
 const authenticateJWT = (req, res, next) => {
-    const authHeader = req.headers.authorization;
+  const authHeader = req.headers.authorization;
 
-    if (authHeader) {
-        const token = authHeader.split(' ')[1];
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    console.log(token);
 
-        jwt.verify(token, accessTokenSecret, (err, user) => {
-            if (err) {
-                return res.sendStatus(403);
-            }
+    jwt.verify(token, accessTokenSecret, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
 
-            req.user = user;
-            next();
-        });
-    } else {
-        res.sendStatus(401);
-    }
-}
+      req.user = user;
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
 
 const books = [
-    {
-        "author": "Chinua Achebe",
-        "country": "Nigeria",
-        "language": "English",
-        "pages": 209,
-        "title": "Things Fall Apart",
-        "year": 1958
-    },
-    {
-        "author": "Hans Christian Andersen",
-        "country": "Denmark",
-        "language": "Danish",
-        "pages": 784,
-        "title": "Fairy tales",
-        "year": 1836
-    },
-    {
-        "author": "Dante Alighieri",
-        "country": "Italy",
-        "language": "Italian",
-        "pages": 928,
-        "title": "The Divine Comedy",
-        "year": 1315
-    },
-]
+  {
+    author: "Chinua Achebe",
+    country: "Nigeria",
+    language: "English",
+    pages: 209,
+    title: "Things Fall Apart",
+    year: 1958,
+  },
+  {
+    author: "Hans Christian Andersen",
+    country: "Denmark",
+    language: "Danish",
+    pages: 784,
+    title: "Fairy tales",
+    year: 1836,
+  },
+  {
+    author: "Dante Alighieri",
+    country: "Italy",
+    language: "Italian",
+    pages: 928,
+    title: "The Divine Comedy",
+    year: 1315,
+  },
+];
 
-app.get('/books', authenticateJWT, (req, res) => {
-    res.json(books);
+app.get("/books", authenticateJWT, (req, res) => {
+  res.json(books);
 });
 
-app.post('/books', authenticateJWT, (req, res) => {
-    const { role } = req.user;
+app.post("/books", authenticateJWT, (req, res) => {
+  const { role } = req.user;
 
-    if (role !== 'admin') {
-        return res.sendStatus(403);
-    }
+  if (role !== "admin") {
+    return res.sendStatus(403);
+  }
 
+  const book = req.body;
+  books.push(book);
 
-    const book = req.body;
-    books.push(book);
-
-    res.send('book added successfully');
+  res.send("book added successfully");
 });
 
 app.listen(4000, () => {
-    console.log('Books service started on port 4000');
+  console.log("Books service started on port 4000");
 });
